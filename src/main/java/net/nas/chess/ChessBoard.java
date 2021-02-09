225d47b4ca6f751c8e7a4f7d41d20e160de5240d
Malloc72P <41663269+Malloc72P@users.noreply.github.com>
2021-02-09 15:33:20 +0900

    ChessBoard클래스에서 체스 보드에 배치된 말의 개수를 구하는 size메서드와 i번째 폰을 찾는 메서드를 구현함

--- src/main/java/net/nas/chess/ChessBoard.java
+++ src/main/java/net/nas/chess/ChessBoard.java
@@ -1,13 +1,25 @@
 package net.nas.chess;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 public class ChessBoard {
-    private ArrayList<ChessPiece> board;
+    final private ArrayList<ChessPiece> board;
     public ChessBoard(){
         board = new ArrayList<ChessPiece>();
     }
     public void addChessPiece(ChessPiece piece){
         board.add(piece);
+    }
+    public int size(){
+        return board.size();
+    }
+    public Pawn findPawn(int i){
+        Optional<Pawn> optionalPawn = board.stream()
+                .filter((piece)-> piece instanceof Pawn)
+                .skip(i)
+                .map(obj->(Pawn)obj)
+                .findFirst();
+        return optionalPawn.orElse(null);
     }
 }
